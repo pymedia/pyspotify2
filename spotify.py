0911daa0c7d60c1a20b3aaa87dac43301559b0dc
pymedia <dborisov@pymedia.org>
2017-08-13 19:40:09 -0700

    Handshake fixed.

--- spotify.py
+++ spotify.py
@@ -27,30 +27,19 @@ except:
 
 KEY_LENGTH=               96
 SPOTIFY_AP_ADDRESS=       ( 'guc3-accesspoint-b-cz1w.ap.spotify.com', 80 )
-SPOTIFY_API_VERSION=      105900395  # Value taken from Windows client
+SPOTIFY_API_VERSION=      0x10800000000
 INFORMATION_STRING=       "pyspotify2"
-DEVICE_ID=                "UNKNOWN"
-VERSION_STRING=           "0.1"
+DEVICE_ID=                "984816fd329622876e14907634264e6f332e9fb3"
+VERSION_STRING=           "pyspotify2-0.1"
+
 LOGIN_REQUEST_COMMAND=    0xAB
+AUTH_SUCCESSFUL_COMMAND=  0xAC
+AUTH_DECLINED_COMMAND=    0xAD
 MAC_SIZE=                 4
 
 class CONNECT_TYPE(enum.Enum):
   CONNECT_TYPE_HANDSHAKE= 1
   CONNECT_TYPE_STREAM=    2
-
-# -------------------------------------------------------------------------------      
-def print_hello_request( hello ):
-  print( 'build_info', hello.build_info )
-  print( 'fingerprints_supported', hello.fingerprints_supported )
-  print( 'cryptosuites_supported', hello.cryptosuites_supported )
-  print( 'powschemes_supported', hello.powschemes_supported )
-  print( 'login_crypto_hello', len( hello.login_crypto_hello.diffie_hellman.gc ), '\n',  hello.login_crypto_hello )
-  print( 'client_nonce', len( hello.client_nonce ), '\n', hello.client_nonce )
-  print( 'padding', len( hello.padding ), '\n', hello.padding )
-  print( 'feature_set', hello.feature_set )
-
-def print_hello_response( ap_resp ):
-  print( 'remote_key gs', ap_resp.challenge.login_crypto_challenge.diffie_hellman.gs )
 
 # ----------------------------------- Plain Connection to server ----------------------------------
 class Connection:
@@ -71,10 +60,11 @@ class Connection:
       request= prefix+ struct.pack(">I", size )+ data
     else:
       self._encoder.set_nonce( self._encoder_nonce )
+      self._encoder_nonce+= 1
+
       request= bytes( [ prefix ] )+ struct.pack(">H", len( data ) )+ data
       request= self._encoder.encrypt( request )
       request+= self._encoder.finish( MAC_SIZE )
-      self._encoder_nonce+= 1
 
     self._socket.send( request )
     return request
@@ -83,13 +73,16 @@ class Connection:
     if self._connect_type== CONNECT_TYPE.CONNECT_TYPE_HANDSHAKE:
       size_packet= self._socket.recv( 4 )
       size= struct.unpack( ">I",  size_packet )
-      return '', size[ 0 ], self._socket.recv( size[ 0 ] )
+      return '', size[ 0 ], size_packet+ self._socket.recv( size[ 0 ]- 4 )
     else:
       self._decoder.set_nonce( self._decoder_nonce )
       self._decoder_nonce+= 1
-      resp_header= self._decoder.decrypt( self._socket.recv( 3 ) )
+
+      recv_header= self._socket.recv( 3 )
+      resp_header= self._decoder.decrypt( recv_header )
       size= struct.unpack( ">H", resp_header[ 1: ] )[ 0 ]
-      resp_body= self._decoder.decrypt( self._socket.recv( size ) )
+      recv_body= self._socket.recv( size )
+      resp_body= self._decoder.decrypt( recv_body )
       return resp_header[ 0 ], size, resp_body
 
   def handshake_completed( self, send_key, recv_key ):
@@ -109,25 +102,21 @@ class Session:
     diffiehellman_hello= keyexchange.LoginCryptoDiffieHellmanHello( **{ 'gc':                self._local_keys.public_key.to_bytes( KEY_LENGTH, byteorder='big' ), \
                                                                         'server_keys_known': 1 } )                                            
 
-    request = keyexchange.ClientHello( **{ 'build_info':             keyexchange.BuildInfo( **{ 'product':  keyexchange.PRODUCT_LIBSPOTIFY_EMBEDDED, 
+    request = keyexchange.ClientHello( **{ 'build_info':             keyexchange.BuildInfo( **{ 'product':  keyexchange.PRODUCT_PARTNER, 
                                                                                                 'platform': keyexchange.PLATFORM_LINUX_X86, 
                                                                                                 'version':  SPOTIFY_API_VERSION } ),
-                                           'fingerprints_supported': [ keyexchange.FINGERPRINT_GRAIN ],
                                            'cryptosuites_supported': [ keyexchange.CRYPTO_SUITE_SHANNON ],
-                                           'powschemes_supported':   [ keyexchange.POW_HASH_CASH ],
                                            'login_crypto_hello':     keyexchange.LoginCryptoHelloUnion( **{ 'diffie_hellman': diffiehellman_hello } ),
                                            'client_nonce' :          bytes( [ int( random.random()* 0xFF ) for x in range( 0, 0x10 ) ] ),
-                                           'padding':                bytes( [ 0 for x in range( 0, 0x1E ) ] ),
+                                           'padding':                bytes( [ 0x1E ] ),
                                            'feature_set':            keyexchange.FeatureSet( **{ 'autoupdate2': True } ) } )
-                                           
     return self._connection.send_packet( b"\x00\x04", 
-                                        request.SerializeToString()  )
+                                         request.SerializeToString()  )
   
   def _processAPHelloResponse( self, init_client_packet ):
     prefix, size, init_server_packet= self._connection.recv_packet()
     response= keyexchange.APResponseMessage()
-    response.ParseFromString( init_server_packet )
-    #print_hello_response(response)
+    response.ParseFromString( init_server_packet[ 4: ] )
     remote_key= response.challenge.login_crypto_challenge.diffie_hellman.gs
     self._local_keys.generate_shared_secret(int.from_bytes(remote_key, 
                                                            byteorder='big'));
@@ -137,13 +126,12 @@ class Session:
     data= []                         
     for i in range( 1, 6 ):
       mac= mac_original.copy()
-      mac.update( bytes(init_client_packet) + bytes( init_server_packet)+ bytes([i]) )
+      mac.update( init_client_packet+ init_server_packet+ bytes([i]) )
       data+= mac.digest()
       
     mac= hmac.new( bytes( data[ :0x14 ] ), 
                    digestmod= hashlib.sha1 )
-    mac.update( bytes(init_client_packet) )
-    mac.update( bytes(init_server_packet) )
+    mac.update( init_client_packet+ init_server_packet )
     
     return ( mac.digest(),
              bytes( data[ 0x14 : 0x34 ] ),
@@ -177,14 +165,18 @@ class Session:
                                                                                                              'system_information_string': INFORMATION_STRING, 
                                                                                                              'device_id':                 DEVICE_ID } ),
                                                                'version_string': VERSION_STRING  } )
-    
     packet= self._connection.send_packet( LOGIN_REQUEST_COMMAND, 
                                           auth_request.SerializeToString() )
     # Get response
     command, size, body= self._connection.recv_packet()
-    print( 'AUTH request', packet, ' len ', len( packet ) )
-    print( 'Resp 0x%x' % command, 'Size %d' % size )
-    print( 'Body', [ hex( x ) for x in body ] )
+    if command== AUTH_SUCCESSFUL_COMMAND:
+      auth_welcome= authentication.APWelcome()
+      auth_welcome.ParseFromString( body )
+      return auth_welcome.reusable_auth_credentials
+    elif command== AUTH_DECLINED_COMMAND:
+      raise Exception( 'AUTH DECLINED. Code: %02X' % command )
+    
+    raise Exception( 'UNKNOWN AUTH CODE %02X' % command )
     
   def __init__( self ):
     # Generate local keys`
@@ -196,10 +188,11 @@ if __name__ == '__main__':
   if len( sys.argv )!= 3:
     print( 'Usage: spotify.py <username> <password>' )
   else:
+    
     connection = Connection()
     session= Session()
     session.connect( connection )
-    session.authenticate( sys.argv[ 1 ], 
-                          bytes( sys.argv[ 2 ], 'ascii' ), 
-                          authentication.AUTHENTICATION_USER_PASS )
-
+    reusable_token= session.authenticate( sys.argv[ 1 ], 
+                                          bytes( sys.argv[ 2 ], 'ascii' ), 
+                                          authentication.AUTHENTICATION_USER_PASS )
+    print( 'AUTH successfull. Token: ', reusable_token )
